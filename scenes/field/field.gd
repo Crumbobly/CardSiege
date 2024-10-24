ec7c535d3822aab1007c186658cd3a250ea2ffe5
Crumbobly <vkvalera004@gmail.com>
2024-10-24 15:51:18 +0300

    Логика распределения карточек в поле была доработана. Проблемы которые были решены: 	Из-за наложения анимаций карты могли не возвращаться на свои места. 	Сигналы убраны и заменены на простой вызов функций (_on_card_selected_for_field и deselected заменены на hand.selected_card; card_on_field и off на вызовы соответсвующих функций из функции set_card_overfield (*))
    	Раньше move_for_fall вызывалась каждый тик, когда карта находилась на поле -> каждый раз создавались tween и стартовали анимации.
    	Сейчас move_for_fall по прежнему вызывается каждый тик, но в нёй проверяется соответсвие координат. И если нет смысла запускать анимации и двигать карты, то мы этого не делаем.
    
    (*) - Как-то не хорошо, что move_for_fall вызывается из set_card_overfield. Стоит как то разделить эту логику.
    Возмоны новые ошибки - следует протестировать.

--- scenes/field/field.gd
+++ scenes/field/field.gd
@@ -2,90 +2,65 @@ extends CardLayout
 class_name Field
 
 
-@onready var shape: BoxShape3D = $Area3D/CollisionShape3D.shape
-var circle_layut = CircleLayoutLogic.new(11.5)
 @onready var collision_shape = $Area3D/CollisionShape3D
+@onready var shape: BoxShape3D = $Area3D/CollisionShape3D.shape
+@onready var hand =  $"../../Hand"
+
+var circle_layut = CircleLayoutLogic.new(11.5)
 var line_layoyt = LineLayoutLogic.new()
-@export var card_size : float = .7
-var on_selected_card : Card3D
-@export var move_offcet_for_drop : float = 1.5
-var m_p
+var card_size : float = .7
+var curr_coords
 
-var for_debug
-
-var is_mouse_over = false
 
 func _ready() -> void:
 	super._ready()
-	$"../../Hand"._on_card_selected_for_field.connect(Callable($".", "collec"))
-	$"../../Hand"._on_card_deselected_for_field.connect(Callable($".", "uncollec"))
-	#$"../../Hand".on_drop_state_field.connect(Callable($".", "move_for_fall"))
-	Events.connect("card_on_field", Callable($".","move_for_fall"))
-	Events.connect("card_off_field", Callable($".", "precalculate_all_card_position"))
-	$Area3D.connect("mouse_entered", Callable($Area3D, "on_mouse_entered"))
-	$Area3D.connect("mouse_exited", Callable($Area3D, "on_mouse_exited"))
-func on_mouse_exited():
-	print("A")
-	is_mouse_over = false
-func on_mouse_entered():
-	is_mouse_over = true
+
+	
 func precalculate_all_card_position():
 	var coords = _get_cards_distribution()
-	if(card_collection != null):
-		recalculate_all_card_position(coords)
+	recalculate_all_card_position(coords)
 	
 	
-func move_for_fall():
-		var coords = _get_cards_distribution()
-		var _move_tween : Tween = create_tween().set_ease(Tween.EASE_OUT).set_parallel(true)
-		print(coords)
-		if(coords != null):
-			adjust_coords_pos(coords)
-			if(on_selected_card != null):
-				for i in len(coords):
-					var card : Card3D = card_collection[i]
-					card.my_tween_list.add_tween(_move_tween)
-					if(on_selected_card.over_field == card.over_field):
-						if(coords[i] <= on_selected_card.over_field_coord_x):
-							coords[i] -= 2	
-						if(coords[i] >= on_selected_card.over_field_coord_x):
-							coords[i] += 2
-						_move_tween.tween_property(card, "position:x", coords[i], 0.2)
+func move_for_fall(hand_selected_card: Card3D):
+	var coords = _get_cards_distribution()
 	
+	if(coords != null):
+		
+		adjust_coords_pos(coords)
+		
+		for i in len(coords):
+			var card : Card3D = card_collection[i]
+			if(coords[i] <= hand_selected_card.over_field_coord_x):
+				coords[i] -= 2
+			if(coords[i] >= hand_selected_card.over_field_coord_x):
+				coords[i] += 2
+		
+		if coords != curr_coords:
+			for i in len(coords):
+				var card : Card3D = card_collection[i]
+				var _move_tween : Tween = create_tween().set_ease(Tween.EASE_OUT).set_parallel(true)
+				_move_tween.tween_property(card, "position:x", coords[i], 0.2)
 
-
-	
-func collec(card : Card3D):
-	on_selected_card = card
-
-	
-
-func uncollec():
-	on_selected_card = null
-
+		curr_coords = coords
 
 
 func recalculate_all_card_position(coords):
 	
-	var scale_tween : Tween = create_tween()
-	var move_tween : Tween = create_tween().set_ease(Tween.EASE_OUT).set_parallel(true)
 	if coords == null:
 		return
 	
-	
 	adjust_coords_pos(coords)	
 	super.recalculate_all_card_position(coords)	
-	
-	for index in range(len(coords)):
-		var card: Card3D = card_collection[index]
-		card.my_tween_list.add_tween(scale_tween)
-		scale_tween.set_ease(Tween.EASE_OUT).set_parallel(true)
-		
-		move_tween.tween_property(card, "position:x", coords[index], 0.2)
-		#card.position.x = coords[index]
-		card.global_position.y = self.position.y
-		scale_tween.tween_property(card, "scale",Vector3(0.4,0.4,0.4), 0.2)
 
+	for index in range(len(coords)):
+		var scale_tween : Tween = create_tween().set_ease(Tween.EASE_OUT).set_parallel(true)
+		var move_tween : Tween = create_tween().set_ease(Tween.EASE_OUT).set_parallel(true)
+		var card: Card3D = card_collection[index]
+		move_tween.tween_property(card, "position:x", coords[index], 0.2)
+		card.global_position.y = self.position.y
+		scale_tween.tween_property(card, "scale", Vector3(0.4, 0.4, 0.4), 0.2)
+
+	curr_coords = coords
 
 func adjust_coords_pos(coord):
 	if(len(coord) % 2 == 0):
