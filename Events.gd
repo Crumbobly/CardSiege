2ad6287cfb01a7dc0e9331fe28c0ca953d1a184b
Crumbobly <vkvalera004@gmail.com>
2024-10-24 15:37:45 +0300

    Events перемещён, но теперь нигде не используется Логика с Events была заменена на простой вызов функций

--- Events.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-signal card_on_field()
-
-signal card_on_drag()
-
-signal card_off_field()
---
+++ scripts/Events.gd
@@ -0,0 +1,5 @@
+extends Node
+
+signal card_on_field(card: Card3D)
+
+signal card_off_field(field: Field)
